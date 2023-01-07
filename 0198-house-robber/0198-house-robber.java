28b7973720d3f12986241fb8aa3ddfec6fb18620
yash-tingre-10 <86177587+yash-tingre-10@users.noreply.github.com>
2023-01-08 00:39:21 +0530

    Time: 1 ms (10.97%), Space: 39.7 MB (64.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,19 +3,37 @@ class Solution {
     public int rob(int[] nums) {
         
         int n = nums.length;
+        // if(n==2)
+        //     return Math.max(nums[0], nums[1]);
+        System.out.println(n);
         dp = new int [n+1];
-        Arrays.fill(dp, -1);
-        return helper(nums, n-1);
+        Arrays.fill(dp, 0);
+        dp[0] = nums[0];
+        // return helper(nums, n-1);
         
+        return helper(nums, 0, n-1);
     }
-    static int helper(int [] arr, int n){
-        if(n<0)
-            return 0;
-        if(n==0)
-            return arr[0];
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n] = Math.max(helper(arr, n-2) + arr[n],helper(arr,n-1) + 0);
+    // Memoizations
+//     static int helper(int [] arr, int n){
+//         if(n<0)
+//             return 0;
+//         if(n==0)
+//             return arr[0];
+//         if(dp[n]!=-1)
+//             return dp[n];
+//         return dp[n] = Math.max(helper(arr, n-2) + arr[n],helper(arr,n-1) + 0);
         
+//     }
+    
+    // Tabulation
+     static int helper(int [] arr, int s, int n){
+        
+         for(int i=1; i<=n; i++){
+            if(i>1) dp[i] = Math.max(dp[i-1],dp[i-2]+arr[i]); 
+            else dp[i] = Math.max(dp[i-1],arr[i]);
+         }
+        
+        
+         return dp[n];
     }
 }
