6e8999c92d8e3c6dffbf8f7ee0bc5eae29c5b46d
yash-tingre-10 <86177587+yash-tingre-10@users.noreply.github.com>
2023-01-09 23:57:27 +0530

    Time: 46 ms (60.51%), Space: 41.9 MB (75.45%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -2,23 +2,36 @@ class Solution {
     static int []dp;
     public int numSquares(int n) {
         dp = new int [n+1];
-        Arrays.fill(dp, -1);
+        Arrays.fill(dp, n+1);
         dp[0] = 0;
         return helper(n);
         
     }
 
-static int helper(int n){
-        if(n==0)
-            return 0;
-        if(dp[n]!=-1)
-            return dp[n];
-        int ans = Integer.MAX_VALUE;
-        for(int i=1; i*i<=n; i++)
-        {
-            ans = Math.min(helper(n-i*i) , ans);
-        }
-        return dp[n] = ans + 1;
+    // Memoization
+// static int helper(int n){
+//         if(n==0)
+//             return 0;
+//         if(dp[n]!=-1)
+//             return dp[n];
+//         int ans = Integer.MAX_VALUE;
+//         for(int i=1; i*i<=n; i++)
+//         {
+//             ans = Math.min(helper(n-i*i) , ans);
+//         }
+//         return dp[n] = ans + 1;
         
+//     }
+    
+    // Tabulation
+    static int helper(int n){
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j*j<=n; j++){
+                if(i-j*j>=0)
+                    dp[i] = Math.min(dp[i-j*j] + 1, dp[i]);
+            }
+        }
+        return dp[n];
     }
 }
