06abc9141ecb6df034a377025bc116777a7fc3b9
yash-tingre-10 <86177587+yash-tingre-10@users.noreply.github.com>
2023-01-11 23:52:38 +0530

    Time: 1 ms (10.66%), Space: 40.1 MB (33.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,12 +3,15 @@ class Solution {
     public int rob(int[] nums) {
         
         int n = nums.length;
-        // if(n==2)
-        //     return Math.max(nums[0], nums[1]);
+        if(n==1)
+            return nums[0];
+        if(n==2)
+            return Math.max(nums[0], nums[1]);
         System.out.println(n);
         dp = new int [n+1];
         Arrays.fill(dp, 0);
         dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
         // return helper(nums, n-1);
         
         return helper(nums, n-1);
@@ -28,9 +31,8 @@ class Solution {
     // Tabulation
      static int helper(int [] arr, int n){
         
-         for(int i=1; i<=n; i++){
-            if(i>1) dp[i] = Math.max(dp[i-1],dp[i-2]+arr[i]); 
-            else dp[i] = Math.max(dp[i-1],arr[i]);
+         for(int i=2; i<=n; i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2]+arr[i]);
          }   
          return dp[n];
     }
