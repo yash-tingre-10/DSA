d49b4b7a3439e2fab207d32b90cc5a47f8860725
yash-tingre-10 <86177587+yash-tingre-10@users.noreply.github.com>
2023-01-15 19:26:48 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (88.67%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -4,28 +4,25 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<Integer> output = new ArrayList<>(); 
         ans = new ArrayList<List<Integer>>();
+        
         helper(nums, output, 0);
         return ans;
     }
     
-    static void helper(int []nums, List<Integer> output, int i)
+    static void helper(int [] nums, List<Integer> output, int i)
     {
         if(i>=nums.length){
             ans.add(new ArrayList(output));
             return;
         }
         
-        
-        // Include
+//         Include
         output.add(nums[i]);
         helper(nums, output, i+1);
         
-        // Exclude
+//         Back-Tracking by removing the element just added
+//         To search more possibilities
         output.remove(output.size()-1);
         helper(nums, output, i+1);
-       
-        
-
-
     }
 }
