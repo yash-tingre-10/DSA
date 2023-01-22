25d7f1ba102cf92dd27df98bf9ba55c48679eeb0
yash-tingre-10 <86177587+yash-tingre-10@users.noreply.github.com>
2023-01-22 18:10:39 +0530

    Time: 1 ms (81.51%), Space: 42.1 MB (40.33%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -21,45 +21,46 @@ class Solution {
             int extra = (maxWidth - currLen - space) % div;
             int wc = j-i;
             if(wc==1 || j>=n)
-                ans.add(justifyLast(words,i,j,maxWidth));
+                ans.add(justifyLeft(words,atleast,extra,i,j,false,maxWidth));
             else
-                ans.add(justifyLeft(words,atleast,extra,i,j));
+                ans.add(justifyLeft(words,atleast,extra,i,j,true,maxWidth));
          i=j;   
         }
         
     return ans;
 }
-    static String justifyLast(String [] words, int i, int j,int maxWidth)
-    {
-        StringBuilder sb = new StringBuilder();
-        for(int p=i; p<j; p++)
-        {
-            sb.append(words[p]);
-            if(p==j-1)
-                break;
-            sb.append(" ");
-        }
-        while(sb.length()<maxWidth)
-            sb.append(" ");
-        return sb.toString();
-    }
     
-    static String justifyLeft(String [] words,int atleast, int extra, int i, int j)
+    static String justifyLeft(String [] words,int atleast, int extra, int i, int j,boolean flag,int maxWidth)
     {
         StringBuilder sb = new StringBuilder();
-        for(int p=i; p<j; p++)
+        if(flag==true)
         {
-            sb.append(words[p]);
-            if(p==j-1)
-                break;
-            sb.append(" ");
-            for(int k=0; k<atleast; k++)
+            for(int p=i; p<j; p++)
+            {
+                sb.append(words[p]);
+                if(p==j-1)
+                    break;
                 sb.append(" ");
-            if(extra>0){
-                sb.append(" ");
-                extra--;
+                for(int k=0; k<atleast; k++)
+                    sb.append(" ");
+                if(extra>0){
+                    sb.append(" ");
+                    extra--;
+                }
             }
         }
+        else
+        {
+            for(int p=i; p<j; p++)
+            {
+                sb.append(words[p]);
+                if(p==j-1)
+                    break;
+                sb.append(" ");
+            }
+            while(sb.length()<maxWidth)
+                sb.append(" ");
+            }
         return sb.toString();
     }
     
