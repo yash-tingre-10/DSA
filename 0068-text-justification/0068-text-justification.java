c7d5b1d63d562ac12e124f438b187992230ea83e
yash-tingre-10 <86177587+yash-tingre-10@users.noreply.github.com>
2023-01-22 18:02:52 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (5.22%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.java
@@ -0,0 +1,69 @@
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        int n=words.length;
+        
+        List<String> ans=new ArrayList<>();
+        int i=0;
+        while(i<n)
+        {
+            int j = i+1;
+            int currLen = words[i].length();
+            while( (j<n) && (currLen + words[j].length() + (j-i-1) ) < maxWidth) 
+            {
+                currLen = currLen + words[j].length();
+                j++;
+            }
+            int div = (j-i-1);
+            int space = (j-i-1);
+            if(div==0)
+                div=1;
+            int atleast = (maxWidth - currLen - space) / div;
+            int extra = (maxWidth - currLen - space) % div;
+            int wc = j-i;
+            if(wc==1 || j>=n)
+                ans.add(justifyLast(words,atleast,extra,i,j,maxWidth));
+            else
+                ans.add(justifyLeft(words,atleast,extra,i,j,maxWidth));
+         i=j;   
+        }
+        
+    return ans;
+}
+    static String justifyLast(String [] words,int atleast, int extra, int i, int j,int maxWidth)
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int p=i; p<j; p++)
+        {
+            sb.append(words[p]);
+            if(p==j-1)
+                break;
+            sb.append(" ");
+        }
+        while(sb.length()<maxWidth)
+            sb.append(" ");
+        return sb.toString();
+    }
+    
+    static String justifyLeft(String [] words,int atleast, int extra, int i, int j,int maxWidth)
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int p=i; p<j; p++)
+        {
+            sb.append(words[p]);
+            if(p==j-1)
+                break;
+            sb.append(" ");
+            for(int k=0; k<atleast; k++)
+                sb.append(" ");
+            if(extra>0){
+                sb.append(" ");
+                extra--;
+            }
+        }
+        return sb.toString();
+    }
+    
+    
+   
+    
+}
