a4e397fb42e3d977447857ea12716102232f4270
yash-tingre-10 <86177587+yash-tingre-10@users.noreply.github.com>
2023-08-02 13:25:08 +0530

    Time: 63 ms (99.92%), Space: 18.2 MB (90.06%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,9 @@
+import itertools 
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return list(itertools.combinations(
+            [
+                i for i in range(1,n+1)
+            ],
+            k
+        ))
